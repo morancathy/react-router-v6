5adc5995581d3dab84d2fb8a63766e149ea0c2f8
Cat <morancathy@gmail.com>
2022-01-28 10:10:35 -0600

    v5 had useHistory

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,41 @@
-import { Route } from "react-router-dom"
+import { Route, useHistory } from "react-router-dom";
 
 // nested routes
-import Offers from "./Offers"
+import Offers from "./Offers";
 
 export default function About() {
+  const history = useHistory();
+
   return (
     <div className="content">
       <h2>About Us</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
-      
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
 
-      <Route path="/about/offers">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
+
+      <button onClick={() => history.push("/products")}>
+        See Our Products
+      </button>
+
+      {/* <Route path="/about/offers">
         <Offers />
-      </Route>
+      </Route> */}
     </div>
-  )
+  );
 }
